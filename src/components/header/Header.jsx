50f55b848c1935e83b3131a2427fe75573259dc1
hoidal <sjhoidal@gmail.com>
2020-09-03 01:08:09 -0600

    added graphql and gatsby image to nav logo"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from 'react'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import logo from '../../images/logo.png'
 import { FaAlignRight } from 'react-icons/fa'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './Header.module.css'
 import LINKS from '../../constants/links'
 
+const getLogoImage = graphql`
+	query logoImage {
+		logoImage: file(relativePath: { eq: "logo.png" }) {
+			childImageSharp {
+				fluid(maxWidth: 600) {
+					...GatsbyImageSharpFluid_tracedSVG
+				}
+			}
+		}
+	}
+`
+
 export default function Header() {
 	const [isOpen, setNav] = useState(false)
 	const toggleNav = () => setNav((isOpen) => !isOpen)
+
+	const { logoImage } = useStaticQuery(getLogoImage)
+	console.log(logoImage)
 
 	return (
 		<nav className={styles.header}>
 			<div className={styles.headerContainer}>
 				<div className={styles.navItems}>
 					<AniLink to="/" fade>
-						<img className={styles.logo} src={logo} alt="Better Hand Books Logo" />
+						<Img
+							className={styles.logo}
+							fluid={logoImage.childImageSharp.fluid}
+							alt="Logo"
+						/>
 					</AniLink>
 					<button type="button" className={styles.menuBtn} onClick={toggleNav}>
 						<FaAlignRight className={styles.menuIcon} />
