45ba375c3b894df19d7f30e628b5a4b9a98bcc6b
hoidal <sjhoidal@gmail.com>
2020-09-04 22:24:15 -0600

    removed console log

--- src/components/book-card/BookCard.jsx
+++ src/components/book-card/BookCard.jsx
@@ -5,11 +5,7 @@ export default function BookCard({ data, handleOpenModal }) {
 	return (
 		// eslint-disable-next-line
 		<div className={styles.card} onClick={(e) => handleOpenModal(e, data)}>
-			<img
-				className={styles.coverImage}
-				src={'https' + data.imageUrl.slice(4)}
-				alt={data.title}
-			/>
+			<img className={styles.coverImage} src={data.imageUrl} alt={data.title} />
 			<h5 className={styles.title}>{data.title}</h5>
 		</div>
 	)
