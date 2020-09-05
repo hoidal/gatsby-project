83001d57447f7307a46b8ad15754fc94af75ff39
hoidal <sjhoidal@gmail.com>
2020-09-04 20:01:36 -0600

    updated http url issue with mixed content

--- src/components/book-card/BookCard.jsx
+++ src/components/book-card/BookCard.jsx
@@ -5,7 +5,11 @@ export default function BookCard({ data, handleOpenModal }) {
 	return (
 		// eslint-disable-next-line
 		<div className={styles.card} onClick={(e) => handleOpenModal(e, data)}>
-			<img className={styles.coverImage} src={data.imageUrl} alt={data.title} />
+			<img
+				className={styles.coverImage}
+				src={'https' + data.imageUrl.slice(4)}
+				alt={data.title}
+			/>
 			<h5 className={styles.title}>{data.title}</h5>
 		</div>
 	)
