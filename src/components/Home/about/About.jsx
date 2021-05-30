bb3155f8d07e606aba596ca5614d847d50893717
Sam Hoidal <sjhoidal@gmail.com>
2021-05-30 13:08:57 -0600

    updated about and process language

--- src/components/Home/about/About.jsx
+++ src/components/Home/about/About.jsx
@@ -31,18 +31,10 @@ export default function About() {
 				<article className={styles.aboutInfo}>
 					<h4>who we are</h4>
 					<p>
-						Welcome to Better Hand Books! Our mission is to create an online platform
-						where book donations will go to where they are needed most. When you donate
-						to us, the books are added to our online platform’s inventory where schools,
-						prisons, community programs and the people who need them most can access and
-						request what they really want and need for free. We accept textbooks,
-						children’s books, professional books, and novels in any condition.
+						Welcome to Better Hand Books! Our mission is to create an online platform where book donations will go to where they are needed most. When you donate to us, the books are added to our online platform’s inventory. Local community and social programs can then access our platform and request the books they really want and need for free. We accept textbooks, children’s books, professional books, and novels in good condition.
 					</p>
 					<p>
-						Right now we are currently doing a trial run to see if this will even work
-						so we are only accepting donations from the Denver metro area. If this
-						initial run proves successful, we will form a nonprofit and expand our
-						outreach as far as possible. Thank you in advance for your support!
+						Right now we are currently a two man operation so we are only accepting donations from the Denver metro area. We will be holding an annual book drive during the month of September to collect books for our inventory which will then be distributed during the rest of the year. Thank you in advance for your support!
 					</p>
 					<AniLink to="/take-action" fade>
 						<button type="button" className="btn-primary">
--- src/constants/process.js
+++ src/constants/process.js
@@ -7,20 +7,20 @@ export default [
 		icon: <FaBookReader />,
 		title: 'donate',
 		text:
-			'Have old and unused books lying around? We accept book donations of all kinds in all conditions. We partner with local institutions to get your books into the hands of those who need them!',
+			'Have old and unused books lying around? We accept book donations of all kinds in good condition. We donate to local institutions to get your books into the hands of those who need them!',
 	},
 	{
 		id: 'request-books',
 		icon: <FaUniversity />,
 		title: 'request books',
 		text:
-			'Are you an organization with readers in need of books? We partner with schools, community programs, prisons, and other institutions to provide books of all types.',
+			'Are you an organization with readers in need of books? We plan to partner with community and social programs to provide books of all types.',
 	},
 	{
 		id: 'coming-soon',
 		icon: <FaRecycle />,
 		title: 'coming soon',
 		text:
-			'We understand that not all books are still in readable condition. As we grow and formalize this passion project, we hope to find a creative way to recycle unusuable books!',
+			'We understand that not all books are still in readable condition. As we grow and formalize this passion project, we hope to find a creative way to recycle unusable books!',
 	},
 ]
